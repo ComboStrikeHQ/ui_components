bf7f8da65f77214ef9d50aec6b7d46be2535add2
Helge Rausch <helge@rausch.io>
2015-05-06 16:54:56 +0200

    Drop _id suffix when looking up translation

--- app/cells/form_cell_base.rb
+++ app/cells/form_cell_base.rb
@@ -4,6 +4,7 @@ class FormCellBase < Cell::ViewModel
   private
 
   def label
-    options[:label] || controller.t(".#{options[:name]}")
+    options[:label] if options.key?(:label)
+    controller.t(".#{options[:name].sub(/_id/, '')}")
   end
 end

d61e017e7fd8010a9836880396205f330a3d58a3
Niklas Schmidtmer <ns@ad2games.com>
2015-05-11 09:38:04 +0200

    Allow passing method via options

--- lib/ui_components/form_helper.rb
+++ lib/ui_components/form_helper.rb
@@ -9,7 +9,13 @@ module UiComponents
     def modelless_form(options, &block)
       model_params = options[:params] || params[options[:name]]
       struct = ValidatedOpenStruct.new(model_params)
-      bootstrap_form_for(struct, as: options[:name], url: options[:url].to_s, &block)
+      bootstrap_form_for(
+        struct,
+        as: options[:name],
+        url: options[:url].to_s,
+        method: options.fetch(:method, :post),
+        &block
+      )
     end
   end
 end

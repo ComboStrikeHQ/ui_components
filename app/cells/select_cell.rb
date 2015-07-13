9a6ec5cb0c1dfe4c4bf048da731b2485b885dc0c
Helge Rausch <helge@rausch.io>
2015-07-13 11:43:01 +0200

    Remove unused methods from SelectCell

--- app/cells/select_cell.rb
+++ app/cells/select_cell.rb
@@ -40,35 +40,9 @@ class SelectCell < FormCellBase
     options.fetch(:name).to_s
   end
 
-  def html_options
-    html_opts = { class: css_class }
-    html_opts.update(options.slice(:required, :multiple))
-    html_opts[:data] = options.slice(:placeholder, :error, :width, :remote_options)
-    html_opts
-  end
-
   def react_options
     options
       .slice(:remote_options, :options, :chosenOptions, :multiple)
       .merge(name: name, id: id, selected: selected)
-  end
-
-  def options
-    unless super[:width]
-      warn 'DEPRECATED: UiComponents here, you did not provide a :width option' \
-        ' to the select component. The default value of "300px" still applies,' \
-        ' but will be dropped at some point in the future in favor of' \
-        ' bootstrap\'s default. Provide a :width option to make this warning go' \
-        ' away.'
-    end
-
-    { width: '300px' }.merge(super)
-  end
-
-  def css_class
-    classes = %w(form-control ui-components-select chosen)
-    classes << 'chosen-inline' if options[:inline]
-    classes << options[:classes] if options[:classes]
-    classes.join(' ')
   end
 end

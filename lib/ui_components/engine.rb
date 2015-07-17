fbaa22db05ccdf692b5e2a953f184917345d80d4
Helge Rausch <helge@rausch.io>
2015-07-17 15:35:20 +0200

    Remove rails-assets-chosen from load assets paths

--- lib/ui_components/engine.rb
+++ lib/ui_components/engine.rb
@@ -2,7 +2,10 @@ module UiComponents
   class Engine < ::Rails::Engine
     initializer 'ui_components.assets.remove_from_load_paths' do |app|
       app.config.assets.paths.reject! do |path|
-        path.match(/rails-assets-react-\d/) # Already in react-rails gem
+        [
+          /rails-assets-react-\d/, # Already in react-rails gem
+          /rails-assets-chosen-\d/ # Already in chosen-rails gem
+        ].any? { |r| path.match(r) }
       end
     end
   end

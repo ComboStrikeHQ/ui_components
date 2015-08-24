54834e1b1dd8d320f560ac7cfc5fd67407967175
Helge Rausch <helge@rausch.io>
2015-08-24 11:47:27 +0200

    Document checkbox list component

--- lib/ui_components/styleguide.rb
+++ lib/ui_components/styleguide.rb
@@ -1,8 +1,7 @@
 module UiComponents
   class Styleguide
     # TODO: Implement cells and remove from here.
-    EXCLUDED_COMPONENTS = %w(ToolbarCell SelectCell
-                             DateRangeCell CheckboxListCell)
+    EXCLUDED_COMPONENTS = %w(ToolbarCell SelectCell DateRangeCell)
 
     def self.components
       Engine.components_paths.map(&:basename)

cc9b48d87443d67adf94ce7db228133ddb55c532
Helge Rausch <helge@rausch.io>
2015-08-25 15:42:45 +0200

    Add test for component generator

---
+++ spec/feature/component_generator_spec.rb
@@ -0,0 +1,23 @@
+RSpec.feature 'Component generator', :js do
+  let(:test_component_path) do
+    UiComponents::Engine.root.join('app', 'cells', 'test_component')
+  end
+
+  before do
+    expect(Dir.exist?(test_component_path)).to be(false)
+    `bundle exec thor generate_component test_component`
+    expect(Dir.exist?(test_component_path)).to be(true)
+    require test_component_path.join('test_component_cell')
+  end
+
+  after do
+    FileUtils.rm_r(test_component_path)
+  end
+
+  scenario 'generates a new component' do
+    visit '/'
+
+    expect(page).to have_content('TestComponentCell')
+    expect(page).to have_content("I didn't change the default description.")
+  end
+end

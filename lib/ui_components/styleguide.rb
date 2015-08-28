5e6f40c97e56bdd65c3a05c24c24b08b61582fa0
Helge Rausch <helge@rausch.io>
2015-08-28 10:16:10 +0200

    Sort components alphabetically in the styleguide

--- lib/ui_components/styleguide.rb
+++ lib/ui_components/styleguide.rb
@@ -2,8 +2,8 @@ module UiComponents
   class Styleguide
     def self.components
       Engine.components_paths.map do |path|
-        (path.basename.to_s.camelize + 'Cell').constantize
-      end
+        path.basename.to_s.camelize + 'Cell'
+      end.sort.map(&:constantize)
     end
   end
 end

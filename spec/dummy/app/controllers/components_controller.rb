ac97992c7996e0f1f8fc914ecb195986ec6e79e7
Helge Rausch <helge@rausch.io>
2015-09-17 15:48:30 +0200

    Test modal component in isolation

--- spec/dummy/app/controllers/components_controller.rb
+++ spec/dummy/app/controllers/components_controller.rb
@@ -2,4 +2,17 @@ class ComponentsController < ApplicationController
   def show
     render params[:name]
   end
+
+  def new_show
+    component_class = "#{params.require(:name)}_cell".camelize.constantize
+    @example = component_class.examples[example_index]
+    fail "Example #{example_index} not found" unless @example
+    render layout: 'single_component'
+  end
+
+  private
+
+  def example_index
+    params.require(:example_index).to_i
+  end
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/' => 'styleguide#components', as: :styleguide_components
   get '/patterns' => 'styleguide#patterns', as: :styleguide_patterns
   get '/select_async_data' => 'search#search'
+  get '/components/*name/:example_index' => 'components#new_show'
   get '/*name' => 'components#show'
 end

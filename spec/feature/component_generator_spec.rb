460045da937756e1e102efb1236a6baa0f68a6ed
Matthew Correia <mco@ad2games.com>
2016-04-05 18:12:13 +0200

    Fix issue with `bundle` not being found on CircleCI

--- spec/feature/component_generator_spec.rb
+++ spec/feature/component_generator_spec.rb
@@ -2,16 +2,15 @@
 require 'English'
 
 RSpec.feature 'Component generator', :js do
+  load 'lib/generators/generate_component.thor'
+
   let(:test_component_path) do
     UiComponents::Engine.root.join('app', 'cells', 'test_component')
   end
 
   before do
     expect(Dir.exist?(test_component_path)).to be(false)
-
-    output = `bundle exec thor generate_component test_component 2>&1`
-    raise output unless $CHILD_STATUS.success?
-
+    GenerateComponent.start(%w(test_component))
     expect(Dir.exist?(test_component_path)).to be(true)
     require test_component_path.join('test_component_cell')
   end
